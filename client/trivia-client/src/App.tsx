79a75c43efcade1d917104702d39ab741b4f1ee2
Maxi49 <91350274+Maxi49@users.noreply.github.com>
2025-04-04 02:31:08 -0300

    Se borro el servidor y se cambio la ruta de la api en el cliente

--- client/trivia-client/src/App.tsx
+++ client/trivia-client/src/App.tsx
@@ -22,4 +22,3 @@ function App() {
   );
 }
 export default App
-// Paz mira -> vos podes mover las routes en el componente game directamente porq tecnicamente a partir de ahi es que se empiezan a renderizar las cosas
